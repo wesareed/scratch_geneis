12436a4833187ad093c87f3fa0f2bc80c576f9a7
wesareed <wes@wesreed.co>
2019-08-01 18:53:11 -0400

    add theme support

--- app/public/wp-content/themes/scratch-genesis/functions.php
+++ app/public/wp-content/themes/scratch-genesis/functions.php
@@ -24,7 +24,33 @@ load_child_theme_textdomain('scratch-genesis');
   *
   * @since 1.0.0
   */
-  
+
  function scratch_genesis_setup() {
+
+    //Define theme constants
+    define( 'CHILD_THEME_NAME', 'Scratch Geneis' );
+    define( 'CHILD_THEME_URL', 'https://github.com/wesareed/scratch_geneis' );
+    define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+    //Add HTML5 markup structure
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+    //Add viewport meta tag for mobile browsers
+    add_theme_support( 'genesis-responsive-viewport' );
+
+    //Add theme support for accessibility
+    add_theme_support(
+        'genesis-accessibility',
+        array(
+            'drop-down-menu',
+            'headings',
+            'rems',
+            'search-form',
+            'skip-links',
+        )
+    );
+
+    //Add theme support for footer widgets
+    add_theme_support( 'genesis-footer-widgets', 3 );
 
  }
