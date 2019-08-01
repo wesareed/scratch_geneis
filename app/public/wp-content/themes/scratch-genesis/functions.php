9f538a5c7dcd46684932aa69dcd7efe59ab30d3b
wesareed <wes@wesreed.co>
2019-08-01 18:13:50 -0400

    adds textdomain and setup functions

--- app/public/wp-content/themes/scratch-genesis/functions.php
+++ app/public/wp-content/themes/scratch-genesis/functions.php
@@ -1,2 +1,29 @@
 <?php 
 
+/**
+ * Theme customizations
+ * 
+ * @package   Scratch Genesis
+ * @author    Wes Reed <wes@wesreed.co>
+ * @link      https://htn-consulting.com
+ * @copyright 2019 Wes Reed
+ * @license   MIT
+ * 
+ */
+
+ //Load child theme textdomain
+load_child_theme_textdomain('scratch-genesis'); 
+ 
+ 
+
+ add_action('genesis_setup', 'scratch_genesis_setup');
+ /**
+  * Theme setup.
+  *
+  * Attach all of the site-wide funtions to the correct hooks and filters. All the functions themselves are defined below this setup function
+  *
+  * @since 1.0.0
+  */
+ function scratch_genesis_setup() {
+
+ }
