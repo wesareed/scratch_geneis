aadfecd44c66821eb17e9c88766d805e280ffc2c
wesareed <wes@wesreed.co>
2019-08-02 14:05:31 -0400

    check to see if widget areas are active

--- app/public/wp-content/themes/scratch-genesis/front-page.php
+++ app/public/wp-content/themes/scratch-genesis/front-page.php
@@ -10,6 +10,29 @@
  * @license   MIT
  * 
  */
+function scratch_genesis_home_page_setup() {
+
+    $home_sidebars = array(
+        'home_welcome'   => is_active_sidebar( 'home-welcome' ),
+        'call_to_action' => is_active_sidebar( 'call-to-action' ), 
+    );
+
+    //Return early if no sidebars are active
+    if( ! in_array( true, $home_sidebars)) {
+        return;
+    }
+
+    //Add home welcome area if "Home Welcome" widget area is active
+    if ($home_sidebars['home_welcome']) {
+        //
+    }
+
+    //Add call to action area if "Call To Action" widget area is active
+    if ($home_sidebars['call_to_action']) {
+        //
+    }
+
+}
 
  genesis();
 
